4d3340b8a9c93d74a5979b116fa6176de80d7165
Tony Headford <tony@binarycircus.com>
2016-11-16 11:25:43 +0000

    Updated pafs_core

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-class ApplicationController < ActionController::Base
+class ApplicationController < PafsCore::ApplicationController
   helper PafsCore::Engine.helpers
    # This is a nested template that ultimately calls the gov uk template layout
   # This allows us to auto insert code into any of the hooks provided by the GDS layout.
-  layout "pafs"
+  layout ->(_) { "pafs" }
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
