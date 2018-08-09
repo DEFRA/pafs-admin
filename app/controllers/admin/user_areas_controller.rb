d67a348f090b1e9deacf15b7f4d9e299fc7a037e
Yomi Colledge <baphled@boodah.net>
2018-08-09 11:02:21 +0100

    Make sure admin are directed to the existing users page when deleting an area.

--- app/controllers/admin/user_areas_controller.rb
+++ app/controllers/admin/user_areas_controller.rb
@@ -3,9 +3,10 @@ class Admin::UserAreasController < ApplicationController
 
   def destroy
     if @user_area = PafsCore::UserArea.find(params[:id])
+      user = @user_area.user
       @user_area.destroy
 
-      redirect_to edit_admin_user_path(current_resource)
+      redirect_to edit_admin_user_path(user)
     end
   end
 end
