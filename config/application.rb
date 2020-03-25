23e6efd974c86b20ef59275f9d2eb92c413a4bcd
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-25 18:10:05 +0000

    [PM-587] Update application to remove deprecated call

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,6 @@ module PafsAdmin
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     # exception handling
     config.exceptions_app = self.routes
   end
