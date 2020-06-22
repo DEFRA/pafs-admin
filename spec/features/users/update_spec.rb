9f9bc39b6c43e55f4342a7cbc4d34b3e6d91dca7
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-06-22 12:59:22 +0100

    [PM-609] Add spec for user update

---
+++ spec/features/users/update_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.feature "Update a user", type: :feature do
+  let(:admin) { create(:admin, :pso) }
+  let(:user) { create(:user, :pso) }
+  let!(:area) { create(:rma_area, name: "North East") }
+
+  before do
+    login_as(admin)
+  end
+
+  context "updating a users area" do
+    scenario "updating the user with a new area" do
+      visit("/admin/users/#{user.id}/edit")
+      select("North East", from: 'Main Area')
+
+      expect do
+        click_on "Save"
+        expect(page).to have_content("Manage users")
+      end.to change { user.reload.updated_at }
+
+      expect(user.reload.areas.map(&:name)).to eql(["North East"])
+    end
+  end
+end
