563024e4cd236ac0ab53a0b354eb3e1ce4f9de8b
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-07-13 13:18:30 +0100

    [PM-604] Update rails

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module PafsAdmin
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults 5.0
 
     # load decorators
     config.to_prepare do
