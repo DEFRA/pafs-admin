0e4bbf6aed8456dfeb88fab1d8a315c376549184
Matt Hall <49390835+MattHallEA@users.noreply.github.com>
2020-07-23 15:02:56 +0100

    [NO-JIRA] Migrate to ruby-defra-alerts (#60)
    * Remove airbrake
    * Add ruby-defra-alerts
    * Add configuration via pafs core gem

---
+++ config/initializers/pafs_core_config.rb
@@ -0,0 +1,8 @@
+PafsCore.configure do |config|
+  # Configure airbrake, which is done via the engine using defra_ruby_alert
+  config.airbrake_enabled = ENV.key?("AIRBRAKE_PROJECT_KEY")
+  config.airbrake_host = ENV["AIRBRAKE_HOST"]
+  config.airbrake_project_key = ENV["AIRBRAKE_PROJECT_KEY"]
+  config.airbrake_blocklist = [/password/i, /authorization/i]
+end
+
