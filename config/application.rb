7f1897572737992f5e146c4c9557c51dfcf6d43d
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-03-09 15:32:16 +0000

    Force logging to file

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,9 @@ module PafsAdmin
 
     # exception handling
     config.exceptions_app = routes
+
+    logger           = ActiveSupport::Logger.new(Rails.root.join("log", "#{Rails.env}.log"))
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)  
   end
 end
