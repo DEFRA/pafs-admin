916be0f61ea045cf15b55a26eff06670af684d61
Leonid Brujev <brujev@gmail.com>
2023-08-15 16:23:57 +0100

    RUBY-2444: Adding Manage Organisations page

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
     end
     resources :confirmations, only: [:index]
     resources :camc3, only: [:show], controller: :camc3
+    resources :organisations, only: %i[index edit]
   end
   devise_for :users, controllers: { passwords: "passwords" }
 
