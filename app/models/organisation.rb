59a3466ea6eb0fa804e3885ddd2e49719349d9ac
Leonid Brujev <brujev@gmail.com>
2023-08-21 11:58:20 +0100

    Adjusting area types for organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Organisation < PafsCore::Area
+  AREA_TYPES = [
+    EA_AREA      = "EA Area",
+    PSO_AREA     = "PSO Area",
+    RMA_AREA     = "RMA"
+  ].freeze
+
   paginates_per 20
 end
