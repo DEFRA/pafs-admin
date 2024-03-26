c8ef5eb9f24126b34318364bc34e566675a7fe07
Leonid Brujev <brujev@gmail.com>
2024-03-26 16:40:32 +0000

    [RUBY-2984] Service to reset expired project end financial years

---
+++ spec/services/reset_expired_project_end_financial_year_service_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ResetExpiredProjectEndFinancialYearService do
+
+  subject(:service) { described_class }
+
+  let!(:project) { create(:project, state: state) }
+  let(:state) { create(:state, state: "draft") }
+
+  describe "#run" do
+    shared_examples "resets project_end_financial_year" do
+      it "sets project_end_financial_year to nil" do
+        service.run
+
+        expect(project.reload.project_end_financial_year).to be_nil
+      end
+    end
+
+    context "when project end FY is expired" do
+      before do
+        project.update(project_end_financial_year: 2022)
+      end
+
+      it_behaves_like "resets project_end_financial_year"
+    end
+
+    context "when project end FY is not expired" do
+      before do
+        project.update(project_end_financial_year: 2032)
+      end
+
+      it "does not modify the project" do
+        expect { service.run }.not_to change(project, :project_end_financial_year)
+      end
+    end
+  end
+end
